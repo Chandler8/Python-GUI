427e86d5ee80a229424293ab71f6f48f7b717310
Chandler L <cbl0015@auburn.edu>
2020-11-29 23:42:48 -0500

    Working on appending files

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 # Test
 
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import Label, filedialog, Text
 import os
 
 root = tk.Tk()
+apps = []
+
+def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File",
+                                            filetypes=(("executables","*.exe"), ("all files", "*.*")))
+
+# apps.append(filename)
+# print(filename)
+# for app in apps:
+#     label = tk.Label(frame, text=app, bg="grey")
+#     label.pack()
+
 
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
@@ -14,8 +30,14 @@ frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
 # Now create a clickable button that is attached to the root, not the workspace. AKA it will sit parallel with the borders of the application
 openFile = tk.Button(root, text="Open File", padx=10,
-                        pady=5, fg="white", bg="#263D42")
+                        pady=5, fg="white", bg="#263D42", command=addApp)
 
 openFile.pack()
+
+# Create 2nd clickable root button that runs our selected applications
+runApps = tk.Button(root, text="Run Apps", padx=10,
+                        pady=5, fg="white", bg="#263D42")
+
+runApps.pack()
 
 root.mainloop()
